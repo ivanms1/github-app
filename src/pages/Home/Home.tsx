4a2c7bff27c44fb0dfb555b9aeee71135e5e87df
Ivan Saldano <ivanms1@gmail.com>
2022-07-02 18:56:57 +0200

    style: add illustration in welcome page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { signIn } from "next-auth/react";
 import { Button, Text } from "@mantine/core";
 
+import WelcomeIllustration from "@/assets/icons/welcome.svg";
+
 import styles from "./Home.module.scss";
 
 function Home() {
   return (
     <div className={styles.Home}>
       <Text className={styles.Title}>Welcome to my Github App</Text>
+      <WelcomeIllustration className={styles.WelcomeSVG} />
       <Button size="xl" onClick={() => signIn("github")}>
         Sign in
       </Button>
