68ab7c9575e13cdf5a166fb75404bbf5d92c0c89
Ivan Saldano <ivanms1@gmail.com>
2022-07-03 08:45:36 +0200

    fix: remove hamburguer menu on mobile in home page

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSession } from "next-auth/react";
 import { Burger, Header as MHeader, MediaQuery, Text } from "@mantine/core";
 
 import useStore from "src/store/store";
@@ -9,19 +10,22 @@ import styles from "./Header.module.scss";
 
 function Header() {
   const open = useStore((state) => state.isNavbarOpen);
+  const session = useSession();
   const handleNavbar = useStore((state) => state.handleNavbar);
   return (
     <MHeader height={60} p="md">
       <div className={styles.Header}>
-        {/* inline styles required by the library */}
-        <MediaQuery largerThan="sm" styles={{ display: "none" }}>
-          <Burger
-            opened={open}
-            onClick={() => handleNavbar(!open)}
-            size="sm"
-            mr="xl"
-          />
-        </MediaQuery>
+        {session.status === "authenticated" && (
+          // inline styles required by the library
+          <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+            <Burger
+              opened={open}
+              onClick={() => handleNavbar(!open)}
+              size="sm"
+              mr="xl"
+            />
+          </MediaQuery>
+        )}
         <LunitLogo className={styles.Logo} />
         <Text>Github App</Text>
       </div>
